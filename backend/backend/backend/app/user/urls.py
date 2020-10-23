056e397b37cc137b89e16399395b1b5e3d18d7e4
Aman <amanyadav@cse.iitb.ac.in>
2020-10-23 20:29:05 +0530

    added requirements.txt in backend

--- backend/backend/backend/app/user/urls.py
+++ backend/backend/backend/app/user/urls.py
@@ -3,7 +3,7 @@ from backend.app.user.views import UserRegistrationView, ChangePasswordView, Use
 
 urlpatterns = [
   url(r'^signup', UserRegistrationView.as_view()),
-  url(r'^signin', UserLoginView.as_view()),
+  url(r'^login', UserLoginView.as_view()),
   url(r'^change_pass', ChangePasswordView.as_view()),
   url(r'^delete_user', DeleteUserView.as_view()),
 ]
