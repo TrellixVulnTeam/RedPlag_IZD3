ee4c925ab327126951c95d5768bb3ccf59a3d702
MananAg007 <mananagarwal007@gmail.com>
2020-10-25 21:19:18 +0530

    css for view profile

--- frontend/src/app/view-profile/view-profile.component.ts
+++ frontend/src/app/view-profile/view-profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
-  styleUrls: ['./view-profile.component.css']
+  styleUrls: ['./view-profile.component.scss']
 })
 export class ViewProfileComponent implements OnInit {
 
