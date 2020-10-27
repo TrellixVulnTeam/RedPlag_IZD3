16e69e5c5583d3606a52d69865ff9c3c59a16fc0
KaustubhDighe <kaustubhdighe1@gmail.com>
2020-10-27 20:32:39 +0530

    file upload/download etc added again

---
+++ files/upload/models.py
@@ -0,0 +1,16 @@
+import uuid
+from django.db import models
+
+def random_filename(instance, filename):
+        extension = filename.split(".")[-1]
+        zip_dir_loc = "{}.{}".format(uuid.uuid4(), extension)
+        return zip_dir_loc
+
+class UploadFile(models.Model):
+	uploaded = models.FileField(upload_to = random_filename)
+	timestamp = models.DateTimeField(auto_now_add = True)
+
+class OutputFile(models.Model):
+        zipfile = models.ForeignKey(UploadFile, on_delete = models.CASCADE)
+        textoutput = models.FileField()
+        
